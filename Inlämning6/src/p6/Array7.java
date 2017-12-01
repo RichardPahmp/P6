00f150c487c174393235adb59e81a8237ce48b8d
Oscar Andersson <paroscarandersson@live.com>
2017-12-01 11:57:40 +0100

    Array7 klar typ?

--- Inlämning6/src/p6/Array7.java
+++ Inlämning6/src/p6/Array7.java
@@ -1,8 +1,40 @@
 package p6;
 
 public class Array7 {
-	private int[] array;
+	private int[] array = new int[7];
 	
+	public Array7(){
+		for(int i = 0; i < array.length; i++){
+			array[i] = 0;
+		}
+	}
 	
-
+	public Array7(Array7 array7){
+		this.array = array7.array;
+	}
+	
+	public Array7(int[] array){
+		this.array = array;
+	}
+	
+	public void setElement(int pos, int value){
+		array[pos] = value;
+	}
+	
+	public int getElement(int pos){
+		return array[pos];
+	}
+	
+	public void setArray(Array7 array7){
+		this.array = array7.array;
+	}
+	
+	public void setArray(int[] array){
+		this.array = array;
+	}
+	
+	public Array7 getArray(){
+		return new Array7(this);
+	}
+	
 }
